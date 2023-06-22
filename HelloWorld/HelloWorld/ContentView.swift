87609c1d3c2ef930ffe88d9d0d3e013339694fbf
freakyimac <freakyimac@gmail.com>
2023-06-23 01:38:55 +0900

    AsyncImage Placeholder2

--- HelloWorld/HelloWorld/ContentView.swift
+++ HelloWorld/HelloWorld/ContentView.swift
@@ -7,6 +7,20 @@
 
 import SwiftUI
 
+extension Image {
+    func imageModifier() -> some View {
+        self.resizable()
+            .scaledToFit()
+    }
+    
+    func iconModifier() -> some View {
+        self.imageModifier()
+            .frame(maxWidth: 128)
+            .foregroundColor(.purple)
+            .opacity(0.5)
+    }
+}
+
 struct ContentView: View {
     
     private let imageURL: String = "https://credo.academy/credo-academy@3x.png"
@@ -18,16 +32,10 @@ struct ContentView: View {
 //        AsyncImage(url: URL(string: imageURL), scale: 3)
         // MARK: - 3. Placeholder
         AsyncImage(url: URL(string: imageURL)) { image in
-            image
-                .resizable()
-                .scaledToFit()
+            image.imageModifier()
         } placeholder: {
             Image(systemName: "photo.circle.fill")
-                .resizable()
-                .scaledToFit()
-                .frame(maxWidth: 128)
-                .foregroundColor(.purple)
-                .opacity(0.5)
+                .iconModifier()
         }
         .padding(40)
     }
