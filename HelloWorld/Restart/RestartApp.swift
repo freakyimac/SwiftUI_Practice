68136fa276b0c72567c0898304598782b33afe93
freakyimac <freakyimac@gmail.com>
2023-06-23 02:16:20 +0900

    RestartApp target 추가

---
+++ HelloWorld/Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by 김동현 on 2023/06/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
