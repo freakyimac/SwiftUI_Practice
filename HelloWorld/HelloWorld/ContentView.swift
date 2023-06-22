370fd211692ccea8de113669694ba1d307318c76
freakyimac <freakyimac@gmail.com>
2023-06-23 01:21:41 +0900

    AsyncImage Placeholder

--- HelloWorld/HelloWorld/ContentView.swift
+++ HelloWorld/HelloWorld/ContentView.swift
@@ -15,7 +15,21 @@ struct ContentView: View {
         // MARK: - 1. Basic
 //        AsyncImage(url: URL(string: imageURL))
         // MARK: - 2. Scale
-        AsyncImage(url: URL(string: imageURL), scale: 3)
+//        AsyncImage(url: URL(string: imageURL), scale: 3)
+        // MARK: - 3. Placeholder
+        AsyncImage(url: URL(string: imageURL)) { image in
+            image
+                .resizable()
+                .scaledToFit()
+        } placeholder: {
+            Image(systemName: "photo.circle.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(maxWidth: 128)
+                .foregroundColor(.purple)
+                .opacity(0.5)
+        }
+        .padding(40)
     }
 }
 
