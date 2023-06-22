2f0911745d699baf343f869f31fc915a7a6f3cc9
freakyimac <freakyimac@gmail.com>
2023-06-23 01:04:22 +0900

    프로젝트 추가

---
+++ HelloWorld/HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by 김동현 on 2023/06/23.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
