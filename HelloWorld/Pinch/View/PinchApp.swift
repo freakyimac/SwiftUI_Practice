5c278a3dd22bc7d64468cc55dc205fa20e87967e
freakyimac <freakyimac@gmail.com>
2023-06-25 03:17:21 +0900

    [Pinch]  1. Pinch target 추가

---
+++ HelloWorld/Pinch/View/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by 김동현 on 2023/06/25.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
