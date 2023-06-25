cd500a34710de0fe6594f5e27b59a35b7743cc2d
freakyimac <freakyimac@gmail.com>
2023-06-25 20:23:40 +0900

    [Pinch] 5. SwiftUI Control Interface

---
+++ HelloWorld/Pinch/View/ControlImageView.swift
@@ -0,0 +1,27 @@
+//
+//  ControlImageView.swift
+//  Pinch
+//
+//  Created by 김동현 on 2023/06/25.
+//
+
+import SwiftUI
+
+struct ControlImageView: View {
+    
+    let icon: String
+    
+    var body: some View {
+        Image(systemName: icon)
+            .font(.system(size: 36))
+    }
+}
+
+struct ControlImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        ControlImageView(icon: "minus.magnifyingglass")
+            .preferredColorScheme(.dark)
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
