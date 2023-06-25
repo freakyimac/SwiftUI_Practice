559840f66db6d964d88b930fe578635f2928beab
freakyimac <freakyimac@gmail.com>
2023-06-26 00:45:18 +0900

    [Fructus] 1. Fructus target 추가

---
+++ HelloWorld/Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by 김동현 on 2023/06/26.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
