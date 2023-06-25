db91918adcc179b149bac6db802e6ab1fa8ad050
freakyimac <freakyimac@gmail.com>
2023-06-25 19:39:35 +0900

    [Pinch] 3. SwiftUI Drag Gesture

--- HelloWorld/Pinch/Screen/ContentView.swift
+++ HelloWorld/Pinch/Screen/ContentView.swift
@@ -12,8 +12,15 @@ struct ContentView: View {
     // MARK: - Properties
     @State private var isAnimating: Bool = false
     @State private var imageScale: CGFloat = 1
+    @State private var imageOffset: CGSize = .zero
     
     // MARK: - Functions
+    func resetImageState() {
+        return withAnimation(.spring()) {
+            imageScale = 1
+            imageOffset = .zero
+        }
+    }
     
     // MARK: - Content
     var body: some View {
@@ -22,11 +29,12 @@ struct ContentView: View {
                 // MARK: - Page Image
                 Image("magazine-front-cover")
                     .resizable()
-                    .aspectRatio(contentMode: .fill)
+                    .aspectRatio(contentMode: .fit)
                     .cornerRadius(10)
                     .padding()
                     .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
                     .opacity(isAnimating ? 1 : 0)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
                     .scaleEffect(imageScale)
                 // MARK: - Tap Gesture
                     .onTapGesture(count: 2) {
@@ -35,11 +43,23 @@ struct ContentView: View {
                                 imageScale = 5
                             }
                         } else {
-                            withAnimation(.spring()) {
-                                imageScale = 1
-                            }
+                            resetImageState()
                         }
                     }
+                // MARK: - Drag Gesture
+                    .gesture(
+                        DragGesture()
+                            .onChanged({ value in
+                                withAnimation(.linear(duration: 1)) {
+                                    imageOffset = value.translation
+                                }
+                            })
+                            .onEnded({ _ in
+                                if imageScale <= 1 {
+                                    resetImageState()
+                                }
+                            })
+                    )
             } // ZStack
             .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
