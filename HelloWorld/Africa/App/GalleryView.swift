415b7d27155f33d2298619f785c16a3322b2a4c0
freakyimac <freakyimac@gmail.com>
2023-07-02 00:56:55 +0900

    [Africa] 11. Let’s create a photo gallery with Grid Layout using SwiftUI

--- HelloWorld/Africa/App/GalleryView.swift
+++ HelloWorld/Africa/App/GalleryView.swift
@@ -8,15 +8,82 @@
 import SwiftUI
 
 struct GalleryView: View {
+    
+    // MARK: - Properteis
+    @State private var selectedAnimal: String = "lion"
+    let animals: [Animal] = Bundle.main.decode("animals.json")
+    let hapticImpact = UIImpactFeedbackGenerator(style: .medium)
+    
+    // Simple Grid Definition
+//    let gridLayout: [GridItem] = [
+//        GridItem(.flexible()),
+//        GridItem(.flexible()),
+//        GridItem(.flexible())
+//    ]
+    
+    // Efficient Grid Definition
+//    let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 3)
+    // Dynamic Grid Layout
+    @State private var gridLayout: [GridItem] = [GridItem(.flexible())]
+    @State private var gridColumn: Double = 3.0
+    
+    func gridSwitch() {
+        gridLayout = Array(repeating: .init(.flexible()), count: Int(gridColumn))
+    }
+    
+    // MARK: - Body
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
-            Text("Gallery")
-        }
+            // MARK: - Grid
+            
+            VStack(alignment: .center, spacing: 30) {
+                // MARK: - Image
+                Image(selectedAnimal)
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                    .overlay(
+                        Circle()
+                            .stroke(Color.white, lineWidth: 1)
+                    )
+                
+                // MARK: - Slider
+                Slider(value: $gridColumn, in: 2...4, step: 1)
+                    .padding(.horizontal)
+                    .onChange(of: gridColumn) { newValue in
+                        gridSwitch()
+                    }
+                
+                 LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                    ForEach(animals) { item in
+                        Image(item.image)
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(Circle())
+                            .overlay(
+                                Circle()
+                                    .stroke(Color.white, lineWidth: 1)
+                            )
+                            .onTapGesture {
+                                selectedAnimal = item.image
+                                hapticImpact.impactOccurred()
+                            }
+                    } // Loop
+                } // LazyVGrid
+                 .animation(.easeIn)
+                 .padding(.horizontal, 10)
+                 .padding(.vertical, 50)
+                 .onAppear {
+                     gridSwitch()
+                 }
+            } // VStack
+        } // Scroll
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(MotionAnimationView())
     }
 }
 
+// MARK: - Preview
 struct GalleryView_Previews: PreviewProvider {
     static var previews: some View {
         GalleryView()
