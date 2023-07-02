6e0e142f03003c1422d5dc98b70b4f20fe2021e6
freakyimac <freakyimac@gmail.com>
2023-07-02 20:26:38 +0900

    [TouchDown] 2. Let’s create a custom navigation bar with a logo

--- HelloWorld/TouchDown/App/ContentView.swift
+++ HelloWorld/TouchDown/App/ContentView.swift
@@ -13,11 +13,21 @@ struct ContentView: View {
     
     // MARK: - Body
     var body: some View {
-        VStack {
-            FooterView()
-                .padding(.horizontal)
-        }
-        .padding()
+        ZStack {
+            VStack {
+                NavigationBarView()
+                    .padding(.horizontal, 15)
+                    .padding(.bottom)
+                    .padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.top)
+                    .background(.white)
+                    .shadow(color: .black.opacity(0.05), radius: 5, x: 0, y: 5)
+                Spacer()
+                FooterView()
+                    .padding(.horizontal)
+            } // VStack
+            .background(colorBackground.ignoresSafeArea(.all, edges: .all))
+        } // ZStack
+        .ignoresSafeArea(.all, edges: .top)
     }
 }
 
---
+++ HelloWorld/TouchDown/View/Home/LogoView.swift
@@ -0,0 +1,41 @@
+//
+//  LogoView.swift
+//  TouchDown
+//
+//  Created by 김동현 on 2023/07/02.
+//
+
+import SwiftUI
+
+struct LogoView: View {
+    // MARK: - Properties
+    
+    // MARK: - Body
+    var body: some View {
+        HStack(spacing: 4) {
+            Text("Touch".uppercased())
+                .font(.title3)
+                .fontWeight(.black)
+                .foregroundColor(.black)
+            
+            Image("logo-dark")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 30, height: 30, alignment: .center)
+            
+            Text("Down".uppercased())
+                .font(.title3)
+                .fontWeight(.black)
+                .foregroundColor(.black)
+        } // HStack
+    }
+}
+
+// MARK: - Preview
+struct LogoView_Previews: PreviewProvider {
+    static var previews: some View {
+        LogoView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
