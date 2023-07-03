161bbc0b9ea3169f4c9b2a873d99154f6bbb3af1
freakyimac <freakyimac@gmail.com>
2023-07-04 01:14:56 +0900

    [Devote] 2. Introduction to Core Data and understand the Xcode's Core Data template

---
+++ HelloWorld/Devote/Utility/Constant.swift
@@ -0,0 +1,20 @@
+//
+//  Constant.swift
+//  Devote
+//
+//  Created by 김동현 on 2023/07/04.
+//
+
+import SwiftUI
+
+// MARK: - Formatter
+let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+// MARK: - UI
+
+// MARK: - UX
